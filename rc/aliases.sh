a88088c2d45cf4e9cea6cae52cf851e2f65b52cc
Vladislav Mamon <arcdeltax@gmail.com>
2020-01-22 20:44:48 +0500

    Add aliases and functions

---
+++ rc/aliases.sh
@@ -0,0 +1,16 @@
+# Upward navigation
+alias ~="cd ~"         # Go home
+alias ..='cd ../'      # Go back 1 level up
+alias ...='cd ../../'  # Go back 2 levels up
+
+# General aliases
+alias cls="clear"                         # I'm used to Windows way of clearing terminal
+alias f='open -a Finder ./'               # Open current directory in Finder
+alias chrome="open -a \"Google Chrome\""  # Open in Chrome
+alias ls="gls -AGlhpX --color=auto"       # GNU ls (long listing)
+alias lc="gls -AGhpX --color=auto"        # GNU ls (column listing)
+alias date="gdate"                        # GNU date
+
+# NPM
+alias npm-globals="npm list -g --depth=0"  # List packages installed globally
+alias npm-locals="npm list --depth=0"      # List packages installed locally (current package)
