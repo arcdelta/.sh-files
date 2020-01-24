523a8265acea3bd89c8735e8cba0e7706bd71aef
Vladislav Mamon <arcdeltax@gmail.com>
2020-01-24 17:51:35 +0500

    Remove `chrome` alias

--- rc/aliases.sh
+++ rc/aliases.sh
@@ -4,12 +4,11 @@ alias ..='cd ../'      # Go 1 level up
 alias ...='cd ../../'  # Go 2 levels up
 
 # General aliases
-alias cls="clear"                         # I'm used to Windows way of clearing terminal
-alias f='open -a Finder ./'               # Open current directory in Finder
-alias chrome="open -a \"Google Chrome\""  # Open in Chrome
-alias ls="gls -AGlhpX --color=auto"       # GNU ls (long listing)
-alias lc="gls -AGhpX --color=auto"        # GNU ls (column listing)
-alias date="gdate"                        # GNU date
+alias cls="clear"                    # I'm used to Windows way of clearing terminal
+alias f='open -a Finder ./'          # Open current directory in Finder
+alias ls="gls -AGlhpX --color=auto"  # GNU ls (long listing)
+alias lc="gls -AGhpX --color=auto"   # GNU ls (column listing)
+alias date="gdate"                   # GNU date
 
 # NPM
 alias npm-globals="npm list -g --depth=0"  # List packages installed globally
