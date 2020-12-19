be2cc09e33d19e8f98560bdb20cc4441806b2849
Vladislav Mamon <arcdeltax@gmail.com>
2020-12-19 07:14:53 +0500

    Adjust `cls` alias & rename `f` to `opn`

--- rc/aliases-general.sh
+++ rc/aliases-general.sh
@@ -6,8 +6,8 @@ alias ..='cd ../'      # -> Go 1 level up
 alias ...='cd ../../'  # -> Go 2 levels up
 
 # General aliases
-alias cls="clear"                    # -> I'm used to Windows way of clearing terminal
-alias f='open -a Finder ./'          # -> Open current directory in Finder
+alias cls="clear && printf '\e[3J'"  # -> I'm used to Windows way of clearing terminal
+alias opn='open -a Finder ./'        # -> Open current directory in Finder
 alias ls="gls -AGlhpX --color=auto"  # -> GNU ls (long listing)
 alias lc="gls -AGhpX --color=auto"   # -> GNU ls (column listing)
 alias date="gdate"                   # -> GNU date
